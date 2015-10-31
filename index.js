ebb68425ade61fb5193b65efbbaf3d4470e4c185
Hermann Yung <hermannyung@gmail.com>
2015-10-31 19:41:13 +0800

    Create the main function

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-module.exports = function(){
-    return 2938
+/**
+ * Get telecode for Chinese
+ * Returns telecode by each character
+ * Returns empty string for that character if not found
+ *
+ * @param  {string} text    Text to be translated
+ * @param  {object} options Options object (optional)
+ * @param  {string} options.codeType Traditional or Simplified Chinese or auto (sc preferred)
+ * @return {Array}         Array of telecode
+ */
+
+var dict = require("./data/dict.json")
+
+module.exports = function(text, options){
+    var codes = []
+
+    options = options || {}
+    options.codeType = options.codeType || "auto"
+
+    for (var i in text){
+        var charCode = text.charCodeAt(i).toString(16).toUpperCase()
+
+        var telecode = dict[charCode]
+
+        if (!telecode) {
+            codes.push("")
+            continue
+        }
+
+        if (options.codeType == "auto"){
+            codes.push(telecode["sc"] || telecode["tc"] || "")
+        } else {
+            codes.push(telecode[options.codeType])
+        }
+    }
+
+    return codes
 }
+
